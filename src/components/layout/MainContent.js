6563ec4af50ed9671ec0f15919f7430dca535524
Callum <callum.git@outlook.com>
2020-06-09 22:17:29 +0100

    Added more photos

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 import img1 from '../../img/vw.jpg'
 import img2 from '../../img/ford.jpg'
+import img3 from '../../img/audi.jpg'
+import img4 from '../../img/lexus.jpg'
+import img5 from '../../img/nissan.jpg'
 
 export default function MainContent() {
     const cardTitle1 = 'Volkswagen Polo 1.8L';
     const cardTitle2 = 'Ford Explorer 2.3L';
+    const cardTitle3 = 'Audi Q7 3.1L';
+    const cardTitle4 = 'Lexus LC 500 3.5L';
+    const cardTitle5 = 'Nisssan Pathfinder 3.7L';
     return (
         <div className="main-grid">
             <div className="card">
@@ -19,23 +25,18 @@ export default function MainContent() {
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
-                <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle1}</div>
+                <img src={img3} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle3}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
-                <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle2}</div>
+                <img src={img4} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle4}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
-                <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle1}</div>
-                <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
-            </div>
-            <div className="card">
-                <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle2}</div>
+                <img src={img5} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle5}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
@@ -49,13 +50,18 @@ export default function MainContent() {
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
-                <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle1}</div>
+                <img src={img3} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle3}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
-                <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">{cardTitle2}</div>
+                <img src={img4} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle4}</div>
+                <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
+            </div>
+            <div className="card">
+                <img src={img5} className="img-card-top" alt=''></img>
+                <div className="card-title">{cardTitle5}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
         </div>
