0bf942e2413136c6a6d8a83dafabdc9129771bcd
Callum <callum.git@outlook.com>
2020-06-09 12:41:15 +0100

    Started responsive design

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -4,56 +4,58 @@ import img1 from '../../img/vw.jpg'
 import img2 from '../../img/ford.jpg'
 
 export default function MainContent() {
+    const cardTitle1 = 'Volkswagen Polo 1.8L';
+    const cardTitle2 = 'Ford Explorer 2.3L';
     return (
         <div className="main-grid">
             <div className="card">
                 <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle1}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle2}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle1}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle2}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle1}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle2}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle1}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle2}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img1} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle1}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
             <div className="card">
                 <img src={img2} className="img-card-top" alt=''></img>
-                <div className="card-title">Volkswagen Polo 1.8L</div>
+                <div className="card-title">{cardTitle2}</div>
                 <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur nulla. Nunc non dignissim turpis, ac iaculis tellus. Mauris scelerisque enim quis laoreet dapibus. Suspendisse sit amet faucibus nisi. Nulla sit amet elit in urna commodo efficitur. Ut pellentesque tincidunt metus, sed bibendum leo auctor cursus.</div>
             </div>
         </div>
