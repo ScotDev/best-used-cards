2398065df652905a0de3b5c591b28d7cbdd948e2
Callum <callum.git@outlook.com>
2020-06-09 15:32:57 +0100

    Started mobile menu

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,27 +4,36 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <ul>
-                <li>
-                    <p>Best Used Cars</p>
-                </li>
-                <li>
-                    <a href="#">New deals</a>
-                </li>
-                <li>
-                    <a href="#">Limited offers</a>
-                </li>
-                <li>
-                    <a href="#">Sale</a>
-                </li>
-                <li>
-                    <a href="#">Testimonials</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-            </ul>
+        <nav className='navbar'>
+            <input type='checkbox'></input>
+            <div className='hamburger'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='menu'>
+                <ul>
+                    <li>
+                        <p>Best Used Cars</p>
+                    </li>
+                    <li>
+                        <a href="#">New deals</a>
+                    </li>
+                    <li>
+                        <a href="#">Limited offers</a>
+                    </li>
+                    <li>
+                        <a href="#">Sale</a>
+                    </li>
+                    <li>
+                        <a href="#">Testimonials</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+
         </nav>
 
     );
