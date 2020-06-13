4d4e9355add694a99fca9c3374797bdc13a957dd
Callum <callum.git@outlook.com>
2020-06-13 19:35:02 +0100

    Began work on second content section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.min.css';
 
 import Navbar from './components/layout/Navbar';
-import ShowcaseCarousel from './components/layout/ShowcaseCarousel';
+import Banner from './components/layout/Banner';
 import MainContent from './components/layout/MainContent';
 import Footer from './components/layout/Footer';
 import Credits from './components/layout/Credits';
@@ -14,7 +14,10 @@ function App() {
       <div className="container">
         <Navbar></Navbar>
         <div className="content">
-          <ShowcaseCarousel></ShowcaseCarousel>
+          <Banner></Banner>
+          <section className="section-heading">
+            <h3>New Arrivals</h3>
+          </section>
           <MainContent></MainContent>
         </div>
         <Footer></Footer>
--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroCarousel() {
     return (
-        <div className="carousel">
+        <div className="banner">
             <div className="text-helper">
                 <h1>Best Used Cars!</h1>
                 <p>All the best used cars at the lowest prices in town!</p>
